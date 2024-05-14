fb21b3c9c8c514cd26001754f49cca8e81606ee7
veerendra <75461749+veertp10@users.noreply.github.com>
2024-05-14 19:52:54 +0530

    Update chatpdf1.py

--- chatpdf1.py
+++ chatpdf1.py
@@ -37,9 +37,8 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 app = Flask(__name__)
 
-# load_dotenv()
-# os.getenv("GOOGLE_API_KEY")
-GOOGLE_API_KEY=AIzaSyDfF4LehGB9NCnMfvCG_Z9EB-UGMDlB9qA
+
+GOOGLE_API_KEY="AIzaSyDfF4LehGB9NCnMfvCG_Z9EB-UGMDlB9qA"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 # Read disease names from disease.txt
